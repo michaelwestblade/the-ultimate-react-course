74668c81e98ee6ec1d50b8643d6c5bbe2ed6c336
michaelwestblade <michaelwestblade@gmail.com>
2025-04-10 15:08:36 -0500

    basic baby state

---
+++ steps/src/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function App() {
+    const [step, setStep] = React.useState(0);
+    const messages = [
+        "Learn React ⚛️",
+        "Apply for jobs 💼",
+        "Invest your new income 🤑",
+    ];
+
+    const incrementStep = () => {
+        if (step < messages.length - 1) {
+            setStep(step + 1);
+        } else {
+            setStep(0);
+        }
+    }
+
+    const decrementStep = () => {
+        if (step > 0) {
+            setStep(step - 1);
+        } else {
+            setStep(messages.length - 1);
+        }
+    }
+
+    return (
+        <div className="App">
+            <div className="steps">
+                <div className="numbers">
+                    {messages.map((_, index) => <div key={index} className={index === step ? "active" : ""}>{index+1}</div>)}
+                </div>
+                <div className="message">{messages[step]}</div>
+
+                <div className="buttons">
+                    <button onClick={decrementStep} style={{backgroundColor: '#7950f2', color: '#fff'}}>Previous</button>
+                    <button onClick={incrementStep} style={{backgroundColor: '#7950f2', color: '#fff'}}>Next</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
