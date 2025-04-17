daeeb3a8e68e5d74edfaf134c22dbf1d62e5b0e6
michaelwestblade <michaelwestblade@gmail.com>
2025-04-17 13:58:35 -0500

    switched to a basic reducer

--- react-quiz/src/Components/DateCounter.tsx
+++ react-quiz/src/Components/DateCounter.tsx
@@ -1,7 +1,23 @@
-import {ReactEventHandler, useState} from "react";
+import {ReactEventHandler, useReducer, useState} from "react";
+
+const reducer = (currentState: number, action: { type: string; payload: number }): number => {
+    switch (action.type) {
+        case 'dec':
+            return currentState - action.payload;
+        case 'inc':
+            return currentState + action.payload;
+        case 'setCount':
+            return action.payload;
+        case 'setStep':
+            return action.payload;
+        default:
+            return currentState;
+    }
+}
 
 function DateCounter() {
-    const [count, setCount] = useState(0);
+    // const [count, setCount] = useState(0);
+    const [count, dispatch] = useReducer(reducer, 0);
     const [step, setStep] = useState(1);
 
     // This mutates the date object.
@@ -10,24 +26,27 @@ function DateCounter() {
 
     const dec = function () {
         // setCount((count) => count - 1);
-        setCount((count) => count - step);
+        //setCount((count) => count - step);
+        dispatch({type: 'dec', payload: step});
     };
 
     const inc = function () {
+        dispatch({type: 'inc', payload: step});
         // setCount((count) => count + 1);
-        setCount((count) => count + step);
+        //setCount((count) => count + step);
     };
 
     const defineCount = function (e: React.ChangeEvent<HTMLInputElement>) {
-        setCount(Number(e.target.value));
+        //setCount(Number(e.target.value));
+        dispatch({type: 'setCount', payload: Number(e.target.value)});
     };
 
     const defineStep = function (e: React.ChangeEvent<HTMLInputElement>) {
-        setStep(Number(e.target.value));
+        dispatch({type: 'setStep', payload: Number(e.target.value)});
     };
 
     const reset = function () {
-        setCount(0);
+        //setCount(0);
         setStep(1);
     };
 
@@ -46,7 +65,7 @@ function DateCounter() {
 
             <div>
                 <button onClick={dec}>-</button>
-                <input value={count} onChange={defineCount} />
+                <input value={count} onChange={defineCount}/>
                 <button onClick={inc}>+</button>
             </div>
 
@@ -58,4 +77,5 @@ function DateCounter() {
         </div>
     );
 }
+
 export default DateCounter;
