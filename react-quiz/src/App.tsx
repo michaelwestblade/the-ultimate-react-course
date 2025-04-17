d903dace7fab8ea988d11f4a5111704a2572a4a0
michaelwestblade <michaelwestblade@gmail.com>
2025-04-17 13:44:25 -0500

    new project to learn useReducer

---
+++ react-quiz/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DateCounter from "./Components/DateCounter";
+
+function App() {
+  return (
+    <div className="App">
+      <DateCounter/>
+    </div>
+  );
+}
+
+export default App;
