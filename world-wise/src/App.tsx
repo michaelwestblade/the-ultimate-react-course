e0b6807e03d93f0e6e7eccc29ed396622cf83f59
michaelwestblade <michaelwestblade@gmail.com>
2025-04-19 14:49:20 -0500

    404 page

--- world-wise/src/App.tsx
+++ world-wise/src/App.tsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProductPage from './pages/Product.tsx';
 import HomePage from './pages/Home.tsx';
 import PricingPage from './pages/Pricing.tsx';
+import NotFoundPage from './pages/NotFoundPage.tsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="product" element={<ProductPage />} />
-        <Route path="" element={<HomePage />} />
         <Route path="pricing" element={<PricingPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
