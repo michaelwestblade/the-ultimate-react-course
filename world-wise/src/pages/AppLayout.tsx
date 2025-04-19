43b5802c224cf45d4e6cb883632a2211de57aa3e
michaelwestblade <michaelwestblade@gmail.com>
2025-04-19 16:26:39 -0500

    addign in map container

--- world-wise/src/pages/AppLayout.tsx
+++ world-wise/src/pages/AppLayout.tsx
@@ -1,10 +1,12 @@
 import SideBar from '../components/SideBar.tsx';
 import styles from './AppLayout.module.css';
+import Map from '../components/Map.tsx';
 
 export default function AppLayout() {
   return (
     <div className={styles.app}>
       <SideBar />
+      <Map />
     </div>
   );
 }
