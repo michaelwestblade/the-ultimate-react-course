f6c19d5158fa4f9502065df95b63e430d60945f9
michaelwestblade <michaelwestblade@gmail.com>
2025-04-20 10:36:35 -0500

    no data and erro state

--- world-wise/src/components/CityList.tsx
+++ world-wise/src/components/CityList.tsx
@@ -2,6 +2,7 @@ import styles from './CityList.module.css';
 import { CityInterface } from '../const.ts';
 import Spinner from './Spinner.tsx';
 import CityItem from './CityItem.tsx';
+import Message from './Message.tsx';
 
 export interface CityListProps {
   cities: CityInterface[];
@@ -9,9 +10,19 @@ export interface CityListProps {
   error: boolean;
 }
 
-export default function CityList({ cities, loading }: CityListProps) {
+export default function CityList({ cities, loading, error }: CityListProps) {
   if (loading) {
     return <Spinner />;
+  }
+
+  if (!cities.length) {
+    return <Message message="Add your first city by clicking on the map" />;
+  }
+
+  if (error) {
+    return (
+      <Message message="There was a problem while trying to fetch cities" />
+    );
   }
 
   return (
