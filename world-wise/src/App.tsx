9df8d94023c895626cba2162703fc5c983385cf1
michaelwestblade <michaelwestblade@gmail.com>
2025-04-20 12:44:05 -0500

    redirect to cities

--- world-wise/src/App.tsx
+++ world-wise/src/App.tsx
@@ -41,7 +41,7 @@ function App() {
         <Route path="product" element={<ProductPage />} />
         <Route path="pricing" element={<PricingPage />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<Navigate to="cities" />} />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={
