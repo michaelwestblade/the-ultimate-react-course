692ec11ceb330b4048a3bb0b6eb97a8c783d91fd
michaelwestblade <michaelwestblade@gmail.com>
2025-04-21 11:33:20 -0500

    theme button

--- atomic-blog/src/components/ThemeButton.tsx
+++ atomic-blog/src/components/ThemeButton.tsx
@@ -1,17 +1,25 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { PostContext, PostContextInterface } from '../contexts/PostContext.tsx';
 
 export default function ThemeButton() {
   const { isFakeDark, setIsFakeDark } =
     useContext<PostContextInterface>(PostContext);
 
+  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
+  useEffect(
+    function () {
+      document.documentElement.classList.toggle('fake-dark-mode');
+    },
+    [isFakeDark]
+  );
+
   const handleClick = () => {
-    if (setIsFakeDark && typeof isFakeDark !== 'undefined') {
+    if (setIsFakeDark && typeof isFakeDark === 'boolean') {
       setIsFakeDark((isFakeDark: boolean) => !isFakeDark);
     }
   };
   return (
-    <button onClick={() => handleClick} className="btn-fake-dark-mode">
+    <button onClick={handleClick} className="btn-fake-dark-mode">
       {isFakeDark ? '☀️' : '🌙'}
     </button>
   );
