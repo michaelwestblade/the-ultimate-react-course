d8dec29287e0a12be28ad02b6ff41361ebdf3a63
michaelwestblade <michaelwestblade@gmail.com>
2025-04-27 15:23:44 -0500

    added a working map with leaflet

--- world-wise/src/components/City.tsx
+++ world-wise/src/components/City.tsx
@@ -36,7 +36,7 @@ function City() {
         <div className={styles.row}>
           <h6>City name</h6>
           <h3>
-            <span>{emoji}</span> {cityName} (Lat: {lat}, Lng: {lng})
+            <span>{emoji}</span> {cityName}
           </h3>
         </div>
 
--- world-wise/src/components/Map.tsx
+++ world-wise/src/components/Map.tsx
@@ -1,19 +1,50 @@
 import styles from './Map.module.css';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { useEffect, useState } from 'react';
+import { LatLngExpression } from 'leaflet';
+import { useCities } from '../contexts/CitiesContext.tsx';
 
 export default function Map() {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState<LatLngExpression>({
+    lat: 51.505,
+    lng: -0.09,
+  });
 
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
+  const lat = parseFloat(searchParams.get('lat') || '51.505');
+  const lng = parseFloat(searchParams.get('lng') || '-0.09');
+
+  useEffect(() => {
+    setMapPosition({
+      lat,
+      lng,
+    });
+  }, [lat, lng]);
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate('form')}>
-      <h1>Map</h1>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
+      <MapContainer
+        className={styles.map}
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker key={city.id} position={city.position}>
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
